8dfc12424bccfc29ecc2f90cc3941bc3d198d98f
Efrén Reyes <efren.reyes@globant.com>
2014-08-06 14:17:02 -0500

    exercise finished

--- NSAPrivacyViolatior/ViewController.m
+++ NSAPrivacyViolatior/ViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate >
 @property (strong, nonatomic) IBOutlet UITextView *myTextView;
-
+@property CLLocationManager *myLocationManager;
 @end
 
 @implementation ViewController
@@ -18,15 +20,68 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	self.myLocationManager = [[CLLocationManager alloc] init];
+    self.myLocationManager.delegate = self;
 }
 - (IBAction)startViolatingPrivacy:(id)sender {
+    [self.myLocationManager startUpdatingLocation];
+    self.myTextView.text = @"Locating you...";
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)reverseGeocode:(CLLocation *)location {
+    CLGeocoder *geoCoder = [CLGeocoder new];
+    [geoCoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        CLPlacemark *placemark = [placemarks firstObject];
+        NSString *address = [NSString stringWithFormat:@"%@ %@ \n%@", placemark.subThoroughfare, placemark.thoroughfare, placemark.locality];
+        self.myTextView.text = [NSString stringWithFormat:@"Found You: %@", address];
+        [self findJailNear:placemark.location];
+    }];
+}
+
+-(void)findJailNear:(CLLocation *) location {
+    MKLocalSearchRequest *request = [MKLocalSearchRequest new];
+    request.naturalLanguageQuery = @"prison";
+    request.region = MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(1, 1));
+    MKLocalSearch *search = [[MKLocalSearch alloc] initWithRequest:request];
+    [search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+        NSArray *mapItems = response.mapItems;
+        MKMapItem *mapItem = [mapItems firstObject];
+        self.myTextView.text = [NSString stringWithFormat:@"You should go to %@", mapItem.name];
+        [self getDirectionsTo:mapItem];
+    }];
+}
+
+-(void)getDirectionsTo:(MKMapItem *) mapItem{
+    MKDirectionsRequest *request = [MKDirectionsRequest new];
+    request.source = [MKMapItem mapItemForCurrentLocation];
+    request.destination = mapItem;
+    MKDirections *directions = [[MKDirections alloc] initWithRequest:request];
+    [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse *response, NSError *error) {
+        MKRoute *route = response.routes.firstObject;
+
+        int stepNumber = 1;
+        NSMutableString *directionsString = [NSMutableString string];
+        for (MKRouteStep *step in route.steps) {
+            NSLog(@"%@", step.instructions);
+            [directionsString appendFormat:@"%d: %@\n", stepNumber, step.instructions];
+            stepNumber++;
+        }
+        self.myTextView.text = directionsString;
+    }];
+}
+
+#pragma mark CLLocationManagerDelegate
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"Error: %@", error);
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    for (CLLocation *location in locations) {
+        self.myTextView.text = @"Location found. Reverse geocoding...";
+        [self reverseGeocode:location];
+        [self.myLocationManager stopUpdatingLocation];
+        break;
+    }
 }
 
 @end
